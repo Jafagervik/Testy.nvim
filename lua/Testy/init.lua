7ee3e7cb075453cafbc746afe9f2a88ed0e7de0c
Jørgen Aleksander Fagervik <jorgenafagervik@gmail.com>
2024-10-13 16:39:36 +0200

    now

--- lua/Testy/init.lua
+++ lua/Testy/init.lua
@@ -1,16 +1,18 @@
-local utils = require("Testy.utils")
 local M = {}
-
-M.show = function()
-	print("jaf show")
-end
-
-M.test = function()
-	utils.test()
-end
 
 M.mvnv = function()
 	return vim.cmd(":! mvn --version")
+end
+
+M.mcsp = function(opts)
+	opts = opts or {
+		threads = 1,
+	}
+	print(opts.threads)
+
+	return vim.cmd(":! mvn -T " .. opts.threads .. " clean spotless:apply package")
+
+	-- vim.keymap.set("n", "<Plug>(MyPluginAction)", function() print("Hello") end, { noremap = true })
 end
 
 return M
